148152d0636d81fde80711f134d4304c0945c575
prosg <learner.in.dev@gmail.com>
2024-08-03 21:22:26 +0200

    Ajout composant Annonces

---
+++ src/components/Annonces/Annonces.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import annonces from "../../data/annonces.json";
+import styles from "./Annonces.module.scss";
+
+export default function Annonces() {
+  return (
+    <div className={styles.annoncesContainer}>
+      {annonces.map((item) => (
+        <Link key={item.id} to={`/logement/${item.id}`}>
+          <div className={styles.annonceCard}>
+            <img src={item.cover} alt={item.title} />
+            <h2>{item.title}</h2>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
