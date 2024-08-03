82f0ea65e77eac5bf66f2ba555f187b2f56814c9
prosg <learner.in.dev@gmail.com>
2024-08-03 21:18:30 +0200

    Modification page NotFound

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import styles from "./NotFound.module.scss";
+
+export default function NotFound() {
+  return (
+    <div className={styles.container}>
+      <h2>404</h2>
+      <p>Oups! La page que vous demandez nexiste pas.</p>
+      <Link to="/" className={styles.Link}>
+        {" "}
+        Retourner sur la page daccueil
+      </Link>
+    </div>
+  );
+}
