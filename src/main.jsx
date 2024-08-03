e9ee25404448dc1e64e7f18450c7f84c830f9b5d
prosg <learner.in.dev@gmail.com>
2024-08-03 21:23:06 +0200

    Misa à jour main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
 import Header from "../src/components/Header/Header.jsx";
-import About from "./components/About/About.jsx";
-import NotFound from "./components/NotFound/NotFound.jsx";
+import About from "./pages/About/About.jsx";
+import NotFound from "./pages/NotFound/NotFound.jsx";
 import Footer from "../src/components/Footer/Footer.jsx";
 import "./index.scss";
 
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Header />
     <Routes>
-      <Route exact path="/" element={<App />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
