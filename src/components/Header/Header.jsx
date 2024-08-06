90ef569bd4713c830239f7914f932dce7d7c04f4
prosg <learner.in.dev@gmail.com>
2024-08-06 15:47:10 +0200

    Mise a jour Header jsx scss & Footer scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./Header.module.scss";
 import logo from "../../assets/images/logo-kasa-orange.svg";
 
 export default function Header() {
+  const location = useLocation();
   return (
     <div className={styles.container}>
-      {/* <header className={`${styles.header}`}> */}
       <header className={styles.header}>
         <img src={logo} alt="Logo Kasa" />
         <nav>
-          <Link to="/">Accueil</Link>
-          <Link to="/about">a propos</Link>
+          <Link
+            to="/"
+            className={`${styles.navLink} ${
+              location.pathname === "/" ? styles.active : ""
+            }`}
+          >
+            Accueil
+          </Link>
+          <Link
+            to="/about"
+            className={`${styles.navLink} ${
+              location.pathname === "/about" ? styles.active : ""
+            }`}
+          >
+            a propos
+          </Link>
         </nav>
       </header>
     </div>
