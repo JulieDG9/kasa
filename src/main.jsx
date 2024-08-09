d9272213e75600937f83a763448982ea2c48abc8
prosg <learner.in.dev@gmail.com>
2024-08-09 12:50:11 +0200

    Ajout route page Accommodation

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.jsx";
+import Accommodation from "./pages/Accommodation/Accommodation.jsx";
 import Header from "../src/components/Header/Header.jsx";
 import About from "./pages/About/About.jsx";
 import NotFound from "./pages/NotFound/NotFound.jsx";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Header />
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/logement/:id" element={<Accommodation />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
